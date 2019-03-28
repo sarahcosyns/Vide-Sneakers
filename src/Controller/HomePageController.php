6f5585e892a29a3fa041219bfe07d15a7803a9b3
sarahcosyns <sarahcosyns1@gmail.com>
2019-03-28 09:19:28 +0100

    Ajout des documents pour la création du projet

---
+++ src/Controller/HomePageController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomePageController extends AbstractController
+{
+    /**
+     * @Route("/home/page", name="home_page")
+     */
+    public function HomePage()
+    {
+        return $this->render('home_page/home_page.html.twig');
+    }
+}
